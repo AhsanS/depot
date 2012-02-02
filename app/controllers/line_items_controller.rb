b9eacc9e554c4d730d5dd9cdd32b4311bbc0d768
Ahsan Sharafuddin <ahsan.sharafuddin@hotmail.com>
2012-02-02 02:10:23 -0500

    Page 125

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -47,7 +47,7 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart}#, notice: 'Line item was successfully created.' }
         format.json { render json: @line_item, status: :created, location: @line_item }
       else
         format.html { render action: "new" }
