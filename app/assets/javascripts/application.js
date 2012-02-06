a42dd6b0429da4f39238fe762ebbb3099ef8fbd7
Ahsan Sharafuddin <ahsan.sharafuddin@hotmail.com>
2012-02-05 19:38:16 -0500

    Ch 11 complete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,5 +19,6 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
+//= require jquery-ui
 //= require jquery_ujs
 //= require_tree .
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -48,6 +48,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_url } #@line_item.cart}#, notice: 'Line item was successfully created.' }
+        format.js   { @current_item = @line_item }
         format.json { render json: @line_item, status: :created, location: @line_item }
       else
         format.html { render action: "new" }
